23650c294f50dfee00b81c165c96db06613559ba
Ray Villalobos <ray@planetoftheweb.com>
2019-06-25 12:06:53 -0400

    cleaned things up and added bootstrap module into our app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-app" class="container">
+    <h4>{{title}}</h4>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "MainApp",
+  data: function() {
+    return {
+      title: "Appointment List"
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
