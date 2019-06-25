a3bb1b8f72e571b5ba65aced799a9b0739c5da1a
Ray Villalobos <ray@planetoftheweb.com>
2019-06-25 14:16:06 -0400

    Using data in our app

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="main-app" class="container">
     <h4>{{title}}</h4>
     <font-awesome-icon icon="plus" class="mr-2"/>Add Appointment
+    <div v-for="(item, i) in appointments" v-bind:key="i">
+      <h4>{{item.petName}}</h4>
+      <p>{{item.aptNotes}}</p>
+    </div>
   </div>
 </template>
 
@@ -12,7 +16,34 @@ export default {
   name: "MainApp",
   data: function() {
     return {
-      title: "Appointment List"
+      title: "Appointment List",
+      appointments: [
+        {
+          petName: "Spot",
+          petOwner: "Constance Smith",
+          aptDate: "2017-07-24 08:30",
+          aptNotes: "This German Shepherd is having some back pain"
+        },
+        {
+          petName: "Goldie",
+          petOwner: "Barot Bellingham",
+          aptDate: "2017-07-22 15:50",
+          aptNotes: "This Goldfish has some weird spots in the belly"
+        },
+        {
+          petName: "Mitten",
+          petOwner: "Hillary Goldwyn",
+          aptDate: "2017-07-21 9:15",
+          aptNotes: "Cat has excessive hairballs"
+        },
+        {
+          petName: "Buffy",
+          petOwner: "Hassum Harrod",
+          aptDate: "2017-07-20 15:30",
+          aptNotes:
+            "This Chihuahua has not eaten for three days and is lethargic"
+        }
+      ]
     };
   },
   components: {
