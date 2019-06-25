5c478b101dd05cb6919f74e613039cd616886a36
Ray Villalobos <ray@planetoftheweb.com>
2019-06-25 12:52:28 -0400

    added font awesome icons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="main-app" class="container">
     <h4>{{title}}</h4>
+    <font-awesome-icon icon="plus" class="mr-2"/>Add Appointment
   </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 export default {
   name: "MainApp",
   data: function() {
     return {
       title: "Appointment List"
     };
+  },
+  components: {
+    FontAwesomeIcon
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import App from './App.vue'
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {
+  faPlus, faMinus, faTrash, faCheck
+} from "@fortawesome/free-solid-svg-icons"
+library.add(faPlus, faMinus, faTrash, faCheck);
+
 Vue.config.productionTip = false
 
 new Vue({
