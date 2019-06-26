3a51344416e460aa7a4201f07bdf686a91d4e786
Ray Villalobos <ray@planetoftheweb.com>
2019-06-26 18:44:44 -0400

    deleted elements

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="main-app" class="container">
     <div class="row justify-content-center">
-      <appointment-list :appointments="appointments"/>
+      <appointment-list :appointments="appointments" @remove="removeItem"/>
     </div>
   </div>
 </template>
 
 <script>
 import AppointmentList from "./components/AppointmentList";
+import _ from "lodash";
 import axios from "axios";
 
 export default {
   name: "MainApp",
   data: function() {
     return {
-      title: "Appointment List",
       appointments: []
     };
   },
@@ -25,6 +25,11 @@ export default {
     axios
       .get("./data/appointments.json")
       .then(response => (this.appointments = response.data));
+  },
+  methods: {
+    removeItem: function(apt) {
+      this.appointments = _.without(this.appointments, apt);
+    }
   }
 };
 </script>
