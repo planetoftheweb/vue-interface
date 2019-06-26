8753e60d37cc707d61739e72a6b6af52f3d33960
Ray Villalobos <ray@planetoftheweb.com>
2019-06-26 10:06:44 -0400

    using axios

--- src/App.vue
+++ src/App.vue
@@ -11,43 +11,23 @@
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import axios from "axios";
 
 export default {
   name: "MainApp",
   data: function() {
     return {
       title: "Appointment List",
-      appointments: [
-        {
-          petName: "Spot",
-          petOwner: "Constance Smith",
-          aptDate: "2017-07-24 08:30",
-          aptNotes: "This German Shepherd is having some back pain"
-        },
-        {
-          petName: "Goldie",
-          petOwner: "Barot Bellingham",
-          aptDate: "2017-07-22 15:50",
-          aptNotes: "This Goldfish has some weird spots in the belly"
-        },
-        {
-          petName: "Mitten",
-          petOwner: "Hillary Goldwyn",
-          aptDate: "2017-07-21 9:15",
-          aptNotes: "Cat has excessive hairballs"
-        },
-        {
-          petName: "Buffy",
-          petOwner: "Hassum Harrod",
-          aptDate: "2017-07-20 15:30",
-          aptNotes:
-            "This Chihuahua has not eaten for three days and is lethargic"
-        }
-      ]
+      appointments: []
     };
   },
   components: {
     FontAwesomeIcon
+  },
+  mounted() {
+    axios
+      .get("./data/appointments.json")
+      .then(response => (this.appointments = response.data));
   }
 };
 </script>
