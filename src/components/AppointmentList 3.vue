65aea3bab84e835459a1ba135e76270c7971f44d
Ray Villalobos <ray@planetoftheweb.com>
2019-06-26 17:55:24 -0400

    moment changes

---
+++ src/components/AppointmentList 3.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div v-for="(item, i) in appointments" v-bind:key="i">
+      <h4>{{item.petName}}</h4>
+      <p>{{item.aptNotes}}</p>
+    </div>
+  </div>
+</template>s
+
+<script>
+export default {
+  name: "AppointmentList",
+  props: ["appointments"]
+};
+</script>
