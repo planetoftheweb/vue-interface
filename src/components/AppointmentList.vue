fc77db4e1ed5284c017f7d2a8e6fadafdc632f3b
Ray Villalobos <ray@planetoftheweb.com>
2019-06-27 12:05:08 -0400

    added an index

--- src/components/AppointmentList.vue
+++ src/components/AppointmentList.vue
@@ -3,8 +3,8 @@
     <div class="list-group list-group-flush">
       <div
         class="list-group-item d-flex align-items-start"
-        v-for="(item, i) in appointments"
-        v-bind:key="i"
+        v-for="item in appointments"
+        :key="item.aptIndex"
       >
         <button class="mr-2 btn btn-sm btn-danger" @click="$emit('remove', item)">
           <font-awesome-icon icon="trash"/>
