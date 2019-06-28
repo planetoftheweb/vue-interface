618811dbc8df940cbaadb2be0dc3c93b98a0a838
Ray Villalobos <ray@planetoftheweb.com>
2019-06-27 23:13:46 -0400

    prepared form data

--- src/components/AddAppointment.vue
+++ src/components/AddAppointment.vue
@@ -6,7 +6,7 @@
       </div>
 
       <div class="card-body" :class="{ 'd-none': hidepanel}">
-        <form id="aptForm">
+        <form id="aptForm" @submit.prevent="requestAdd">
           <div class="form-group form-row">
             <label class="col-md-2 col-form-label text-md-right" for="petName">Pet Name</label>
             <div class="col-md-10">
@@ -16,6 +16,7 @@
                 name="petName"
                 id="petName"
                 placeholder="Pet's Name"
+                v-model="formData.petName"
               >
             </div>
           </div>
@@ -23,18 +24,30 @@
           <div class="form-group form-row">
             <label class="col-md-2 col-form-label text-md-right" for="ownerName">Pet Owner</label>
             <div class="col-md-10">
-              <input type="text" class="form-control" id="ownerName" placeholder="Owner's Name">
+              <input
+                type="text"
+                class="form-control"
+                id="ownerName"
+                placeholder="Owner's Name"
+                v-model="formData.ownerName"
+              >
             </div>
           </div>
 
           <div class="form-group form-row">
             <label class="col-md-2 col-form-label text-md-right" for="aptDate">Date</label>
             <div class="col-md-4">
-              <input type="date" class="form-control" id="aptDate">
+              <input type="date" class="form-control" id="aptDate" v-model="formData.aptDate">
             </div>
             <label class="col-md-2 col-form-label text-md-right" for="aptTime">Time</label>
             <div class="col-md-4">
-              <input type="time" class="form-control" name="aptTime" id="aptTime">
+              <input
+                type="time"
+                class="form-control"
+                name="aptTime"
+                id="aptTime"
+                v-model="formData.aptTime"
+              >
             </div>
           </div>
 
@@ -48,6 +61,7 @@
                 name="aptNotes"
                 id="aptNotes"
                 placeholder="Appointment Notes"
+                v-model="formData.aptNotes"
               ></textarea>
             </div>
           </div>
@@ -68,11 +82,22 @@ export default {
   name: "AddAppointment",
   data() {
     return {
+      formData: [],
       hidepanel: true
     };
   },
   components: {
     FontAwesomeIcon
+  },
+  methods: {
+    requestAdd: function() {
+      this.formData = {
+        petName: this.formData.petName,
+        petOwner: this.formData.ownerName,
+        aptDate: this.formData.aptDate + " " + this.formData.aptTime,
+        aptNotes: this.formData.aptNotes
+      };
+    }
   }
 };
 </script>
